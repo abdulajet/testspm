73c1a7c63e132785dd1f6aecc8f1eaf05620ee05
Gady Rozin <gady.rozin@vonage.com>
2018-09-27 17:05:54 +0300

    [NO VAR] jenkinsPipelineTest -4

--- StitchTestApp/ViewController.m
+++ StitchTestApp/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     int jenkinsPipelineTest = 0;
-    jenkinsPipelineTest = 3;
+    jenkinsPipelineTest = 4;
 
 }
 
