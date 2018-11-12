5724c3391c58ff62bb843c903dd16b2671961279
Chen Lev <chen.lev@vonage.com>
2018-11-12 18:58:02 +0200

    no var - update release scripts

--- utils/increase_build_number.sh
+++ utils/increase_build_number.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-FILE=./stitchObjc.podspec
+FILE=./StitchCore.podspec
+
 VERSION=`cat $FILE  | ggrep  s.version| ggrep -oP '(?<=").*?(?=")'`
 NUM_OF_DOTS=`cat $FILE | ggrep  s.version| ggrep -oP '(?<=").*?(?=")' | ggrep -o "\."| wc -l`
 
@@ -20,5 +21,12 @@ fi
 
 gsed -i.bak "/.*s.version/s/$VERSION/$NEW_VERSION/" $FILE
 
+
+#increase the version of stitch client
+FILE_Client=../StitchClient.podspec
+gsed -i.bak "/.*s.version/s/$VERSION/$NEW_VERSION/" $FILE_Client
+
+
+echo $NEW_VERSION
 
 echo $NEW_VERSION
