88aca69915a78b4954b38d9b1b159015938b1a0c
Chen Lev <chen.lev@vonage.com>
2018-11-14 12:27:19 +0200

    no var - fix increase_build_number

--- utils/increase_build_number.sh
+++ utils/increase_build_number.sh
@@ -5,7 +5,7 @@ FILE=./StitchCore.podspec
 VERSION=`cat $FILE  | ggrep  s.version| ggrep -oP '(?<=").*?(?=")'`
 NUM_OF_DOTS=`cat $FILE | ggrep  s.version| ggrep -oP '(?<=").*?(?=")' | ggrep -o "\."| wc -l`
 
-
+echo "VERSION" $VERSION
 NEW_VERSION="$VERSION.$1"
 
 
@@ -15,5 +15,7 @@ gsed -i.bak "/.*s.version/s/$VERSION/$NEW_VERSION/" $FILE
 #increase the version of stitch client
 FILE_Client=./StitchClient.podspec
 gsed -i.bak "/.*s.version/s/$VERSION/$NEW_VERSION/" $FILE_Client
+gsed -i.bak "/.*s.dependency 'StitchCore'/s/$VERSION/$NEW_VERSION/" $FILE_Client
+
 
 echo "NEW_VERSION" $NEW_VERSION
