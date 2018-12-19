79d147d91b9e3013f3598d64ba8c85843d604f00
Chen Lev <chen.lev@vonage.com>
2018-12-19 18:17:49 +0200

    CSI-952 - fix swift app

--- NexmoSwiftApp/AppDelegate.swift
+++ NexmoSwiftApp/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        var client = NexmoClient()
+        var client = NXMClient()
 
         // Override point for customization after application launch.
         return true
