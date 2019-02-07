e0ad6f48ec12348085300a5d984bb2e592800a92
Chen Lev <chen.lev@vonage.com>
2019-02-07 17:09:37 +0200

    no var - fix publish scripts

--- Utils/publish_public.sh
+++ Utils/publish_public.sh
@@ -7,10 +7,10 @@ pod trunk push NexmoClient.podspec
 #create git tag
 INFO_PLIST_FILE="$PWD/../NexmoClient/Info.plist"
 SDK_VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" $INFO_PLIST_FILE)
-MAJOR_VERSION=$(echo $PLIST_VERSION | cut -d. -f1)
-MINOR_VERSION=$(echo $PLIST_VERSION | cut -d. -f2)
+MAJOR_VERSION=$(echo $SDK_VERSION | cut -d. -f1)
+MINOR_VERSION=$(echo $SDK_VERSION | cut -d. -f2)
 
-TAG_NAME="Public/${MAJOR_VERSION}.${MINOR_VERSION}/v$SDK_VERSION"
+TAG_NAME="public/${MAJOR_VERSION}.${MINOR_VERSION}/v$SDK_VERSION"
 
 echo "Marking the repo with tag $TAG_NAME"
 git tag $TAG_NAME
