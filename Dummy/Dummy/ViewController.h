b2b2eca5b586c3fbeac8f55ff40711d8a9b3214c
Madalin Valceleanu <devmobile@vmadalin.com>
2020-05-14 08:57:40 +0100

    Update Dummy/Dummy/ViewController.h
    Co-authored-by: Nicola <horothesun@users.noreply.github.com>

--- Dummy/Dummy/ViewController.h
+++ Dummy/Dummy/ViewController.h
@@ -1,15 +1,5 @@
-//
-//  ViewController.h
-//  Dummy
-//
-//  Created by Sergei Rastrigin on 12/05/2020.
-//  Copyright © 2020 Vonage. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface ViewController : UIViewController
 
-
 @end
-
