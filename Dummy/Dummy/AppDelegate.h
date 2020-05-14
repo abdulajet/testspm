d15f906762dba6c3ee481fa77f70bbea1aac463b
Madalin Valceleanu <devmobile@vmadalin.com>
2020-05-14 08:59:15 +0100

    Update Dummy/Dummy/AppDelegate.h
    Co-authored-by: Nicola <horothesun@users.noreply.github.com>

--- Dummy/Dummy/AppDelegate.h
+++ Dummy/Dummy/AppDelegate.h
@@ -1,15 +1,5 @@
-//
-//  AppDelegate.h
-//  Dummy
-//
-//  Created by Sergei Rastrigin on 12/05/2020.
-//  Copyright © 2020 Vonage. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-
 @end
-
