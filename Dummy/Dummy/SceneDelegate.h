0afafe001fbf00e7c66c356933e8292612d9ef2e
Madalin Valceleanu <devmobile@vmadalin.com>
2020-05-14 08:58:31 +0100

    Update Dummy/Dummy/SceneDelegate.h
    Co-authored-by: Nicola <horothesun@users.noreply.github.com>

--- Dummy/Dummy/SceneDelegate.h
+++ Dummy/Dummy/SceneDelegate.h
@@ -1,16 +1,7 @@
-//
-//  SceneDelegate.h
-//  Dummy
-//
-//  Created by Sergei Rastrigin on 12/05/2020.
-//  Copyright © 2020 Vonage. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
 
 @property (strong, nonatomic) UIWindow * window;
 
 @end
-
