a0376c70a93ef6239cc2f00042de233da6a0671e
horothesun <nicola.dipol@vonage.com>
2020-05-20 23:11:52 +0100

    CSI-1210 - Xcode version switched to v11.3.1 to run Jazzy v0.13.3

--- Dummy/appcenter-post-build.sh
+++ Dummy/appcenter-post-build.sh
@@ -25,10 +25,13 @@ sed -e "s^###SDK_PUBLIC_VERSION###^$PUBLIC_VERSION^g" \
     -e "s^###COPYRIGHT_YEAR###^$COPYRIGHT_YEAR^g" \
     Utils/README_md.template > Utils/README.md
 
-# IMPORTANT: jazzy v0.13.3 DOESN'T SUPPORT Xcode >11.3.1
-# Update AppCenter build settings accordingly
 sudo gem install jazzy -v 0.13.3
 
+# IMPORTANT: jazzy v0.13.3 DOESN'T SUPPORT Xcode >11.3.1
+echo "1️⃣ Xcode path: $(xcode-select -p)"
+XCODE_SELECT_PATH=$(xcode-select -p)
+sudo xcode-select --switch /Applications/Xcode_11.3.1.app/Contents/Developer
+echo "2️⃣ Xcode path: $(xcode-select -p)"
 jazzy --objc --author Vonage \
     --author_url https://developer.nexmo.com \
     --module-version $PUBLIC_VERSION \
@@ -37,8 +40,12 @@ jazzy --objc --author Vonage \
     --module NexmoClient \
     --output docs \
     --readme Utils/README.md
+sudo xcode-select --switch $XCODE_SELECT_PATH
+echo "3️⃣ Xcode path: $(xcode-select -p)"
 
 (cd ./docs; zip -rX docs.zip *)
+
+echo "4️⃣ Docs zipped"
 
 SFTP_URL="nexmo-sdk-ci@s-15a7bf753d804d299.server.transfer.eu-west-1.amazonaws.com"
 SFTP_BASE_PATH="/nexmo-conversation/nexmo-sdk-ci/iOS-SDK/SDK-release-internal/branches/${APPCENTER_BRANCH}/build-id/${APPCENTER_BUILD_ID}"
